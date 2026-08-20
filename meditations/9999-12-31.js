5c1ed4f921b7389ef4251c86497fc76e1d2e96a3
TheRealMarcusChiu <marcuschiu9@gmail.com>
2026-08-20 09:24:12 -0500

    admin: update 9999-12-31

--- meditations/9999-12-31.js
+++ meditations/9999-12-31.js
@@ -7,14 +7,35 @@
   date: "9999-12-31",
   hidden: true,
   body:
-`- to not tear up when talking about what closest to my heart
-- (Uber Driver) going through divorce
-- teejay sis college, recently got promoted
+`Me:
+- me to know the truth and spend time with you
+- to not tear up when talking about what's closest in my heart
+- seeing and reflecting character of God
+- talk to God about them before to them about God
+
+Others:
+- TJ sis college, recently got promoted
 - Peter feel direct impact (would you want to make the greatest impact but not be able to see it, or the smallest impact and be able to see it)
 - Chris struggle with purpose in work
-- Jason with preparing with engagement
-- Jason responding in a gentle manner within work
-- Damien upcoming Engedi BS
-- seeing and reflecting character of T
-- talk to T about them before to them about T`,
+- Jason N with preparing with engagement
+- Jason K responding in a gentle manner within work
+- Damien upcoming Engedi BS - to not only lead but to worship you
+- Mariah to know the truth
+- Stan to know the truth
+- (Uber Driver) going through divorce
+- Orko to know the truth
+- Joy L carry out your grace as a 5th grade teacher
+- Ian give him strength and vigor in his UTD/SMU ministry
+- Jeff H sister going through loss and grief
+- Kim H for her health and grace to speak out the wisdom that you given her
+- Mica H to be bold in speech (stutter and/or shyness?)
+- Aleah to know the will God set for her life
+- Bridget as she trains for half marathon (continue to use her to foster community at church)
+- Tim as he figures out the questions of work (Every Good Endeavor)
+- Will to find treasure in your word over the things under the sun
+- Lydia to not worry or identity in work
+- Josiah to find opportunity to depend on you as he job searches
+- Jordan TODO
+- Charles G (Autozone) as he finds community in a church
+- Ritvik (Pizza) as he places value in searching Truth`,
 });
